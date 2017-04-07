7a85051494245c686a979b9e1cbdad550a4e33b3
Damir Galimov <drenatovich@gmail.com>
2017-04-07 20:52:39 +0200

    Pruebas con inicio.java

--- src/controlador/Inicio.java
+++ src/controlador/Inicio.java
@@ -3,19 +3,15 @@ package controlador;
 import java.awt.EventQueue;
 
 import Pantallas.Difficulty;
+import Pantallas.Menu;
 import Pantallas.Score;
 
 public class Inicio {
 
 	
 	public static void main(String[] args) {
-		
-			
-		Difficulty dif = new Difficulty();
-		dif.setVisible(true);
-		
-		System.out.println("ME LA PONE TIESAPACO ");
-				
+		Menu menu = new Menu();
+		menu.setVisible(true);
 	}
 
 	
