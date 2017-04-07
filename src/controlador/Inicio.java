5f252c1ab61a29f63ddf613be4baa27f604a9e87
JorgeYYA <ies25a.jorgeq@gmail.com>
2017-04-07 20:48:23 +0200

    Cambios en Inicio-java

--- src/controlador/Inicio.java
+++ src/controlador/Inicio.java
@@ -7,27 +7,18 @@ import Pantallas.Score;
 
 public class Inicio {
 
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+		
+			
 					Difficulty dif = new Difficulty();
 					dif.setVisible(true);
 					
 					Score sco = new Score();
 					sco.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+				
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 
 }
